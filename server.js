bdd5f2caf36d5551707e9b04428050be770caa15
keshawn0998 <ksharper@studentmba.org>
2021-11-21 10:10:46 -0500

    cors install

--- server.js
+++ server.js
@@ -7,24 +7,24 @@ const cors = require('cors');
 
 server.use(helmet());
 server.use(express.json());
-server.use(function (req, res, next) {
+// server.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', "*");
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', "*");
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 server.use(cors());
 server.use('/', dataRouter);
 

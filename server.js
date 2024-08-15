300828233a79779d4734edf5c6cb89e4ce659232
KESHAWN SHARPER <ksharper@studentmba.org>
2024-08-15 10:08:54 -0400

    hcfj

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ require('aws-sdk/lib/maintenance_mode_message').suppress = true;
 // const routes = require("./router/route-config")
 
 const cors = require('cors');
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
 let loginRouter = require('./router/authRouters/login-router')
 let registerRouter = require('./router/authRouters/register-router')
 let userRouter = require('./router/user-router')
@@ -25,7 +30,7 @@ function createServer() {
       next();
     });
     server.set({'Access-Control-Allow-Origin': '*'})
-    server.use(cors());
+    server.use(cors(corsOptions));
     server.use('/login',loginRouter);
     server.use('/users',userRouter);
     server.use('/register',registerRouter);

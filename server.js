4966a9b6c953f3bc6053edcaca858bb102496b30
KESHAWN SHARPER <ksharper@studentmba.org>
2024-08-16 15:05:02 -0400

    cors9

--- server.js
+++ server.js
@@ -24,19 +24,18 @@ function createServer() {
     server.use(cors(corsOptions));
     server.use(helmet());
     server.use(express.json());
-    server.use((res,req,next) => {
-      {
-        res.header('Access-Control-Allow-Credentials', true)
-     res.header('Access-Control-Allow-Origin', '*')
-     // another common pattern
-     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-     res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-     res.header(
-       'Access-Control-Allow-Headers',
-       'Origin,Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
-     next()
-   }
-    })
+    app.use(function(req, res, next) {
+      // res.header("Access-Control-Allow-Origin", "*");
+      const allowedOrigins = ['http://localhost:3000', 'https://movie-application-bxf3.vercel.app/', 'https://gamebrag.onrender.com'];
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+           res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      res.header("Access-Control-Allow-credentials", true);
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+      next();
+    });
     server.use('/login',loginRouter);
     server.use('/users',userRouter);
     server.use('/register',registerRouter);

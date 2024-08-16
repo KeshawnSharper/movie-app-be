8a26d1e1e252603a3f080f06ded1da19e821e3ee
KESHAWN SHARPER <ksharper@studentmba.org>
2024-08-15 21:09:56 -0400

    cors4

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
 const serverless = require('serverless-http')
 const createServer = require('./server')
+const cors = require("cors")
+const corsOptions = {
+  credentials: true,
+  origin: "*"// Whitelist the domains you want to allow
+};
 
 const PORT = process.env.PORT || 5001
 const server = createServer();
+server.use(cors(corsOptions));
+
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`)
 })
